6514be8f12ef4f14bd182cc5522a4e0162ff0666
Lucio Hernandez <luciohdz0@gmail.com>
2022-12-13 12:13:30 -0600

    comentarios en configs/server

--- src/configs/server.js
+++ src/configs/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = 3001;
 
-
-module.exports.createServer = function(){
-    app.listen(PORT,()=>console.log('localhost:3001'));
+module.exports.createServer = function () {
+    app.listen(PORT, () => console.log("localhost:3001"));
+    //  MIDDLEWARES
+    //  express-json()
+    //    funciona para poder interpretar los archivos json recibidos
+    //  cors ({options})
+    //    define que IPS pueden conectarse al servidor, agregando una capa de seguridad al servidor.
     app.use(express.json());
     app.use(cors());
-    return app
-}
+    return app;
+};
