2a61d7079ce1ffb8eb3adad0e9dfa0b0b3b650b1
Lucio Hernandez <luciohdz0@gmail.com>
2023-01-10 22:48:07 -0600

    Version prueba deploy

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./index.js",
+    target: "node",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
